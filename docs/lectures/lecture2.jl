e82d09799489a42e6b38343c7ece2eff866b3b86
Kevin Bonham <kevbonham@gmail.com>
2020-06-03 16:02:37 -0400

    add lecture 2

---
+++ docs/lectures/lecture2.jl
@@ -0,0 +1,30 @@
+#nb # %% slideshow [markdown] {"slideshow": {"slide_type": "slide"}}
+# # Lecture 2 - Types and Functions
+# 
+# **Essential Skills For Computational Biology**  
+# *2020-06-08*  
+# Kevin Bonham, PhD
+
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "slide"}}
+# ## A program is data and operations on data
+
+#nb %% A slide [code] {"slideshow": {"slide_type": "fragment"}}
+chromosomes = 23 # this is data
+
+function diploid(n) 
+    return n * 2 # this is an operation
+end
+#-
+#nb %% A slide [code] {"slideshow": {"slide_type": "fragment"}}
+diploid(chromosomes) # this is a program
+
+##nb # %% A slide [markdown] {"slideshow": {"slide_type": "subslide"}}
+# ## How to run a julia program
+#
+# - From the REPL
+# - From a script (Live demo)
+# - From a file inside VS Code (Live demo)
+
+#nb # %% A slide [markdown] {"slideshow": {"slide_type": "fragment"}}
+# [Placeholder for video link](#)
+
